dca9dc2227b706e921e5e8de6a7e1b38ed864861
0xCurtis <curtis1337wastaken@gmail.com>
2025-11-06 18:08:18 +0100

    polymarket url fix

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ ARBITRAGE_THRESHOLD = 0.04
 
 FETCH_INTERVAL_SECONDS = 10
 
-POLYMARKET_API_URL = "https://gamma-api.polymarket.com/markets?limit=100&active=true"
+POLYMARKET_API_URL = "https://gamma-api.polymarket.com/markets?limit=100&closed=false"
 KALSHI_API_URL = "https://api.elections.kalshi.com/trade-api/v2/markets"
 
